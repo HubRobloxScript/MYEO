bafd0ce842aab30c21cf89136bdfa1e7f82d836d
HubRobloxScript <147968044+HubRobloxScript@users.noreply.github.com>
2023-12-05 16:12:44 +0700

    Add files via upload

---
+++ MooYeo.lua
@@ -0,0 +1,23 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("MYENO_X", "DarkTheme")
+
+local Tab = Window:NewTab("others")
+local Section = Tab:NewSection("Main")
+Section:NewButton("infinite-yield", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+Section:NewToggle("super-Human", "Go fast", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+        print("Turn On")
+    else
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+        print("Turn off")
+    end
+end)
+Section:NewSlider("Walkspeed", "Slider get fast", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+Section:NewSlider("Walkspeed", "Slider get fast", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
